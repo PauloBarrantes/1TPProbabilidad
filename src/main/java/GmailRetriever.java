0afeabf217f7a4d4114ee7acc7577fe0951931f0
Berta Sánchez Jalet <nosarabs@gmail.com>
2017-11-17 22:52:49 -0600

    Revert "documentation"
    This reverts commit 46bcff26c2ae632df349ccf26b12c72420f3be7d.

--- src/main/java/GmailRetriever.java
+++ src/main/java/GmailRetriever.java
@@ -33,39 +33,61 @@ public class GmailRetriever {
 
     }
 
-    protected Gmail service;
-    protected String user = "me";
     /**
      * Application name.
      */
+
+    protected Gmail service;
+    protected String user = "me";
     private static final String APPLICATION_NAME = "BayesianSpamFilter";
+    /** Directory to store user credentials for this application. */
     private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"), ".credentials/BayesianSpamFilter");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
     private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
     private static HttpTransport HTTP_TRANSPORT;
+
+    /** Global instance of the scopes required by this quickstart.
+     *
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/gmail-java-quickstart
+     */
     private static final List<String> SCOPES = Arrays.asList(GmailScopes.GMAIL_READONLY);
 
    static {
        try {
           HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-          DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+          DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR); // Está linea da problemas
       } catch (Throwable t) {
         t.printStackTrace();
         System.exit(1);
     }
    }
 
+<<<<<<< HEAD
     /**
      * Creates credentials given that the user has a allowed the app to do so.
      *
      * @return credential
      * @throws IOException
      */
+=======
+
+>>>>>>> parent of 46bcff2... documentation
     private static Credential authorize() throws IOException {
+        // Load client secrets.
 
         InputStream in = GmailRetriever.class.getResourceAsStream("/client_secret.json");
+
+
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
+        // Build flow and trigger user authorization request.
 
         GoogleAuthorizationCodeFlow flow =
                 new GoogleAuthorizationCodeFlow.Builder(
@@ -79,32 +101,39 @@ public class GmailRetriever {
         System.out.println(
                 "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
         return credential;
+
     }
 
+<<<<<<< HEAD
     /**
      * Authorises the credentials through the Gmail Services.
      *
      * @return Gmail.Builder
      * @throws IOException
      */
+=======
+>>>>>>> parent of 46bcff2... documentation
     public static Gmail getGmailService() throws IOException {
         Credential credential = authorize();
+
         return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME).build();
     }
 
-    /**
-     * Links the user to the credentials, through the Gmail Services.
-     * @throws IOException
-     */
     public void logIn() throws IOException {
       user = "me";
       service = getGmailService();
     }
 
+
+
     /**
+<<<<<<< HEAD
      * Creates a path to were the credentials had been saved, and proceeds to delete them afterwards, that way the user succesfully logs out.
      *
      * @throws IOException
+=======
+     *
+>>>>>>> parent of 46bcff2... documentation
      */
     public void logOut() throws IOException {
         String path = System.getProperty("user.home") + "/.credentials/BayesianSpamFilter/StoredCredential";
@@ -143,7 +172,7 @@ public class GmailRetriever {
               break;
           }
       }
-        // The array we are using  has a size of 160, our spam folder has only 33 messages.
+        // El array que estamos usando con mensajes tiene un tamaño como de 160, y en mi SPAM solo hay 33 mensajes
       int counter = 0;
     for (Message message : messages) {
         try {
